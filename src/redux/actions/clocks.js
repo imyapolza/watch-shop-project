4ac36305342345d48d8f3e0508355069cfdebdba
imyapolza <imyapolza7@gmail.com>
2022-02-08 20:17:30 +0500

    Update clocks.js

--- src/redux/actions/clocks.js
+++ src/redux/actions/clocks.js
@@ -9,9 +9,9 @@ export const fetchClocks = (category, sortBy) => (dispatch) => {
   dispatch(setLoaded(false));
   axios
     .get(
-      `http://localhost:3001/clocks?${category != null ? `category=${category}` : ``}&_sort=${
-        sortBy.type
-      }&_order=${sortBy.order}`,
+      `/clocks?${category != null ? `category=${category}` : ``}&_sort=${sortBy.type}&_order=${
+        sortBy.order
+      }`,
     )
     .then(({ data }) => {
       dispatch(setClocks(data));
